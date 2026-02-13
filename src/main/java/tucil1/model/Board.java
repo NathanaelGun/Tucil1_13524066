79373704efb9d440189a100edfff76af8a18ea95
Nathanael <nathanifolia@gmail.com>
2026-02-14 01:36:21 +0700

    feat: define function for model

---
+++ src/main/java/tucil1/model/Board.java
@@ -0,0 +1,31 @@
+package tucil1.model;
+
+public class Board {
+    private int size;
+    private char[][] cells;
+    private int[] queenPositions;
+
+    public boolean isSafe(int row, int col) {
+
+    }
+
+    public void placeQueen(int row, int col) {
+
+    }
+
+    public void removeQueen(int row) {
+
+    }
+
+    public void reset() {
+
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public int[] getQueenPosition() {
+        return queenPositions;
+    }
+}
---
+++ src/main/java/tucil1/solver/Solver.java
@@ -0,0 +1,5 @@
+package tucil1.solver;
+
+public class Solver {
+    
+}
