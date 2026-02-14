acadcbda360330ea1a6d68814793e18718c3f48a
Nathanael <nathanifolia@gmail.com>
2026-02-15 00:46:06 +0700

    feat: add draft for the solver

--- src/main/java/tucil1/solver/Solver.java
+++ src/main/java/tucil1/solver/Solver.java
@@ -1,5 +1,29 @@
 package tucil1.solver;
 
+import tucil1.model.Board;
+
 public class Solver {
-    
+    private Board board;
+
+    public Solver(Board board) {
+        this.board = board;
+    }
+
+    public boolean solve(int row) {
+        if (row == board.getSize()) {
+            return true;
+        }
+        for (int col = 0; col < board.getSize(); col++) {
+            if (board.isSafe(row, col)) {
+                board.placeQueen(row, col);
+                if (solve(row + 1)) {
+                    return true;
+                }
+                else {
+                    board.removeQueen(row);
+                }
+            }
+        }
+        return false;
+    }
 }
